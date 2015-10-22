2a3523ca72401c20d2f5352b2795c593dbab2349
arczmobica <artur.czajkowski@mobica.com>
2015-10-22 14:16:19 +0200

    Added Is_Even class template in order to try and use parallel_invoke function from ppl

--- Concurrency/Concurrency.cpp
+++ Concurrency/Concurrency.cpp
@@ -8,25 +8,103 @@ int f_int()
 	return 1;
 }
 
+using file_content = string;
+
+file_content read_file(string file_name)
+{
+	ifstream f_in(file_name);
+	if (f_in)
+	{
+		string content;
+		string line;
+		while (std::getline(f_in, line))
+		{
+			content += line;
+		}
+		return content;
+	}
+	else
+	{
+		throw Bad_File_Read_Exception{file_name};
+	}
+
+}
+
+template<char letter>
+int count_occurence_of(file_content str)
+{
+	int res = std::count(cbegin(str), cend(str), letter);
+	return res;
+}
+
+string f()
+{
+	return string{};
+}
+
+int real_callback(int val)
+{
+	cout << "Necessary time passed: " << val << '\n';
+	val *= 2;
+	return -1;
+}
+
+template<bool true_type>
+struct Is_Even
+{
+	static const bool is_even{ true };
+	static int accumulate(int val)
+	{
+		cout << "Adding even numbers\n";
+		if (!(val%2))
+		{
+			return val;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+};
+template<>
+struct Is_Even<false>
+{
+	static const bool is_even{ false };
+	static int accumulate(int val)
+	{
+		cout << "Adding odd numbers\n";
+		if ((val % 2))
+		{
+			return val;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+};
+
+template<class Result_T,class Type, class Iterator>
+Result_T accumulate_numbers(Iterator beg, Iterator end)
+{
+	Result_T result{};
+	Result_T intermediate{0};
+	while (beg != end)
+	{
+		result += Type::accumulate(*beg);
+		++beg;
+	}
+	return result;
+}
+
+
 int main()
 {
-	task<int> aTask{create_task(f_int)};
-	if (aTask.is_done())
-	{
-		cout << "Done, but how?\n";
-	}
-	else
-	{
-		cout << aTask.get();
-	}
-	if (aTask.is_done())
-	{
-		cout << "Done, everything as expected\n";
-	}
-	else
-	{
-		cout << "Not done, something isn't right.";
-	}
+	vector<int> vec{1,2,3,4,5,6,7,8,9,10};
+	parallel_invoke([&vec] {accumulate_numbers<int, Is_Even<true>>(cbegin(vec), cend(vec)); },
+					[&vec] { accumulate_numbers<int, Is_Even<false>>(cbegin(vec), cend(vec)); }
+																	);
+
     return 0;
 }
 
